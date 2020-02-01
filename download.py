e59a9b71c9ad02c9343b8dd285fbc7154b58da6a
Luis Parravicini <lparravi@gmail.com>
2020-02-01 10:11:30 -0300

    loop over hours of the day and tiles whitelist

--- download.py
+++ download.py
@@ -19,26 +19,36 @@ image_url = "http://himawari8.nict.go.jp/img/D531106/%dd/%d/%s_%d_%d.png"
 date_format = "YYYY/DD/mm/HHMMSS"
 
 # time is (supposedly) gmt+8
-d = datetime.fromisoformat('2020-01-07 06:00')
 
-base_dir = Path('cache', 'himawari8', d.strftime('%Y-%m-%d'), str(depth))
-base_dir.mkdir(parents=True, exist_ok=True)
+whitelist = (
+    (1, 1),
+)
+print(f'tiles whitelist: {whitelist}')
 
-date = d.strftime('%Y/%m/%d/%H%M%S')
+for h in range(24):
+    d = datetime.fromisoformat('2020-01-28 %02d:00' % h)
 
-for x in range(depth):
-    for y in range(depth):
-        file_name = Path(base_dir, '%d-%d.png' % (x, y))
+    base_dir = Path('cache', 'himawari8', d.strftime('%Y-%m-%d'), str(depth))
+    base_dir.mkdir(parents=True, exist_ok=True)
 
-        url = image_url % (depth, size, date, y, x)
-        print(f'downloading from {url}')
+    date = d.strftime('%Y/%m/%d/%H%M%S')
 
-        if file_name.exists():
-            print('cached, skipping download')
-            continue
+    for x in range(depth):
+        for y in range(depth):
+            if len(whitelist) > 0 and (x, y) not in whitelist:
+                continue
 
-        tmp_fname = file_name.with_suffix('.tmp')
-        with urllib.request.urlopen(url) as response, open(tmp_fname, 'wb') as out_file:
-            shutil.copyfileobj(response, out_file)
+            file_name = Path(base_dir, '%d-%d.png' % (x, y))
 
-        tmp_fname.rename(file_name)
+            if file_name.exists():
+                print(f'tile ({x},{y}) cached, skipping download')
+                continue
+
+            url = image_url % (depth, size, date, y, x)
+            print(f'downloading from {url}')
+
+            tmp_fname = file_name.with_suffix('.tmp')
+            # with urllib.request.urlopen(url) as response, open(tmp_fname, 'wb') as out_file:
+            #     shutil.copyfileobj(response, out_file)
+
+            # tmp_fname.rename(file_name)
