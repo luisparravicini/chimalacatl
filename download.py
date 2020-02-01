50ad4752d537ad93ce5afc0f64e299bdd7af7d9a
Luis Parravicini <lparravi@gmail.com>
2020-02-01 19:48:54 -0300

    refactored download method and creates strip from each row

--- download.py
+++ download.py
@@ -2,6 +2,7 @@ import urllib.request
 import shutil
 from datetime import datetime, timedelta
 from pathlib import Path
+from PIL import Image, ImageDraw
 
 #
 # Downloads a whole day of Himawari 8 images at the specified depth.
@@ -13,6 +14,48 @@ from pathlib import Path
 #
 # There's a json with the date of the latest image taken at:
 # http://himawari8-dl.nict.go.jp/himawari8/img/D531106/latest.json
+
+
+def tile_fname(dir, pos):
+    return Path(dir, '%d-%d.png' % pos)
+
+
+def download_tile(tile):
+    x, y = tile
+    file_name = tile_fname(date_dir, tile)
+
+    if file_name.exists():
+        print(f'\t[{x},{y}] found cache')
+        return
+
+    url = image_url % (depth, size, date_str, y, x)
+    print(f'\t[{x},{y}] downloading from {url}')
+
+    tmp_fname = file_name.with_suffix('.tmp')
+    with urllib.request.urlopen(url) as response, open(tmp_fname, 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+
+    tmp_fname.rename(file_name)
+
+
+def make_strip(date_dir, tiles):
+    strip_fname = Path(date_dir, 'strip_%02d.jpg' % x)
+    if strip_fname.exists():
+        print('\tstrip file exists')
+        return
+
+    print(f'\tsaving strip ({len(tiles)} tiles)')
+    img = Image.new('RGB', (len(tiles) * size, size))
+    for tile in tiles:
+        tile_img = Image.open(tile_fname(date_dir, tile))
+        img.paste(
+            tile_img,
+            (tile[1] * size, 0)
+        )
+    tmp_strip_fname = strip_fname.with_suffix('.tmp')
+    img.save(tmp_strip_fname, format='jpeg')
+    tmp_strip_fname.rename(strip_fname)
+
 
 # depth can be: 4, 8, 16, 20
 # (according to https://habr.com/ru/sandbox/99937/)
@@ -48,23 +91,16 @@ while cur_date < end_date:
     date_dir.mkdir(parents=True, exist_ok=True)
 
     for x in range(depth):
+        tiles = []
         for y in range(depth):
             if len(whitelist) > 0 and (x, y) not in whitelist:
                 continue
 
-            file_name = Path(date_dir, '%d-%d.png' % (x, y))
+            tile = (x, y)
+            tiles.append(tile)
+            download_tile(tile)
 
-            if file_name.exists():
-                print(f'\t[{x},{y}] found cache')
-                continue
+        make_strip(date_dir, tiles)
 
-            url = image_url % (depth, size, date_str, y, x)
-            print(f'\t[{x},{y}] downloading from {url}')
-
-            tmp_fname = file_name.with_suffix('.tmp')
-            with urllib.request.urlopen(url) as response, open(tmp_fname, 'wb') as out_file:
-                shutil.copyfileobj(response, out_file)
-
-            tmp_fname.rename(file_name)
 
     cur_date += step
