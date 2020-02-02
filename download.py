678e13ce406a976b35ca36251a09cf6655dbe4ea
Luis Parravicini <lparravi@gmail.com>
2020-02-02 07:35:13 -0300

    made several of the functions' arguments into instance variables

--- download.py
+++ download.py
@@ -20,19 +20,23 @@ from PIL import Image, ImageDraw
 
 
 class Downloader:
-    def tile_fname(self, dir, pos):
-        return Path(dir, '%d-%d.png' % pos)
+    def __init__(self, create_annotated, force_creation):
+        self.create_annotated = create_annotated
+        self.force_creation = force_creation
 
-    def download_tile(self, date_dir, tile, image_url, depth, size, date_str):
+    def _tile_fname(self, pos):
+        return Path(self.date_dir, '%d-%d.png' % pos)
+
+    def _download_tile(self, tile, image_url, depth, date_str):
         x, y = tile
-        file_name = self.tile_fname(date_dir, tile)
+        file_name = self._tile_fname(tile)
 
         if file_name.exists():
-            print(f'\t[{x},{y}] found cache')
+            self._log(f'[{x},{y}] found cache')
             return
 
-        url = image_url % (depth, size, date_str, y, x)
-        print(f'\t[{x},{y}] downloading from {url}')
+        url = image_url % (depth, self.size, date_str, y, x)
+        self._log(f'[{x},{y}] downloading from {url}')
 
         tmp_fname = file_name.with_suffix('.tmp')
         with urllib.request.urlopen(url) as response, open(tmp_fname, 'wb') as out_file:
@@ -40,36 +44,34 @@ class Downloader:
 
         tmp_fname.rename(file_name)
 
-    def make_strip(self, date_dir, tiles, size, x, force_creation=False, annotated=False):
+    def _make_strip(self, tiles, x, annotated=False):
         if annotated:
-            strip_type = 'annotated'
+            strip_type = 'annotated '
         else:
             strip_type = ''
 
-        strip_fname = Path(date_dir, 'strip_%s_%02d.jpg' % (strip_type, x))
+        strip_fname = Path(self.date_dir, 'strip_%s_%02d.jpg' % (strip_type, x))
         if strip_fname.exists():
-            print(f'\t{strip_type} strip file exists')
-            if force_creation:
-                print('\trecreating strip')
-            else:
+            self._log(f'{strip_type}strip file exists (recreate? {self.force_creation})')
+            if not self.force_creation:
                 return strip_fname
 
-        print(f'\tsaving {strip_type} strip ({len(tiles)} tiles)')
-        width = len(tiles) * size
-        img = Image.new('RGB', (width, size))
+        self._log(f'saving {strip_type}strip ({len(tiles)} tiles)')
+        width = len(tiles) * self.size
+        img = Image.new('RGB', (width, self.size))
         if annotated:
             draw = ImageDraw.Draw(img)
         for index, tile in enumerate(tiles):
-            tile_img = Image.open(self.tile_fname(date_dir, tile))
-            img.paste(tile_img, (index * size, 0))
+            tile_img = Image.open(self._tile_fname(tile))
+            img.paste(tile_img, (index * self.size, 0))
             if annotated:
                 draw.rectangle(
-                    ((index * size, 0), (index * size + size, size)),
+                    ((index * self.size, 0), (index * self.size + self.size, self.size)),
                     width=2
                 )
                 offset = 50
                 draw.text(
-                    (tile[1] * size + offset, offset),
+                    (tile[1] * self.size + offset, offset),
                     f'{tile[0]},{tile[1]}',
                     fill='white'
                 )
@@ -79,32 +81,34 @@ class Downloader:
 
         return strip_fname
 
-    def make_target_image(self, date_dir, cur_date, strips_paths, width, size, force_creation=False):
-        dstr = cur_date.strftime('%Y%m%d%H%M%S')
-        image_fname = Path(date_dir, 'target-%s.jpg' % dstr)
+    def _make_target_image(self, strips_paths, width):
+        dstr = self.cur_date.strftime('%Y%m%d%H%M%S')
+        image_fname = Path(self.date_dir, 'target-%s.jpg' % dstr)
         if image_fname.exists():
-            print('\ttarget image file exists')
-            if force_creation:
-                print('\trecreating image')
-            else:
+            self._log(f'target image file exists (recreate? {self.force_creation})')
+            if not self.force_creation:
                 return
 
-        print(f'\tsaving target ({len(strips_paths)} strips)')
+        self._log(f'saving target ({len(strips_paths)} strips)')
 
-        img = Image.new('RGB', (width, len(strips_paths) * size))
+        img = Image.new('RGB', (width, len(strips_paths) * self.size))
         for index, strip_path in enumerate(strips_paths):
             strip_img = Image.open(strip_path)
-            img.paste(strip_img, (0, index * size))
+            img.paste(strip_img, (0, index * self.size))
 
         tmp_target_fname = image_fname.with_suffix('.tmp')
         img.save(tmp_target_fname, format='jpeg')
         tmp_target_fname.rename(image_fname)
 
-    def main(self, create_annotated=True, force_creation=False):
+    def _log(self, what):
+        when = self.cur_date.strftime('%Y-%m-%dT%H:%M')
+        print(f'{when}: {what}')
+
+    def run(self):
         # depth can be: 4, 8, 16, 20
         # (according to https://habr.com/ru/sandbox/99937/)
         depth = 20
-        size = 550
+        self.size = 550
         image_url = "http://himawari8.nict.go.jp/img/D531106/%dd/%d/%s_%d_%d.png"
 
         target = (
@@ -114,20 +118,18 @@ class Downloader:
         print(f'tiles target: {target}')
 
         step = timedelta(minutes=10)
-        cur_date = datetime.fromisoformat('2020-01-29 00:00')
-        end_date = cur_date + timedelta(days=1)
+        self.cur_date = datetime.fromisoformat('2020-01-29 00:00')
+        end_date = self.cur_date + timedelta(days=1)
 
-        base_dir = Path('~', 'cache-sat', 'himawari8', str(depth), cur_date.strftime('%Y-%m-%d'))
+        base_dir = Path('~', 'cache-sat', 'himawari8', str(depth), self.cur_date.strftime('%Y-%m-%d'))
         base_dir = base_dir.expanduser()
         base_dir.mkdir(parents=True, exist_ok=True)
 
-        while cur_date < end_date:
-            print(cur_date.strftime('time: %H:%M'))
+        while self.cur_date < end_date:
+            date_str = self.cur_date.strftime('%Y/%m/%d/%H%M%S')
 
-            date_str = cur_date.strftime('%Y/%m/%d/%H%M%S')
-
-            date_dir = Path(base_dir, cur_date.strftime('%H-%M'))
-            date_dir.mkdir(parents=True, exist_ok=True)
+            self.date_dir = Path(base_dir, self.cur_date.strftime('%H-%M'))
+            self.date_dir.mkdir(parents=True, exist_ok=True)
 
             strips = []
             for x in range(depth):
@@ -142,19 +144,19 @@ class Downloader:
                     tile = (x, y)
                     tiles.append(tile)
 
-                    self.download_tile(date_dir, tile, image_url, depth, size, date_str)
+                    self._download_tile(tile, image_url, depth, date_str)
 
                 if len(tiles) > 0:
-                    strip_fname = self.make_strip(date_dir, tiles, size, x, force_creation=force_creation)
+                    strip_fname = self._make_strip(tiles, x)
                     strips.append(strip_fname)
-                    if create_annotated:
-                        self.make_strip(date_dir, tiles, size, x, annotated=True)
+                    if self.create_annotated:
+                        self._make_strip(tiles, x, annotated=True)
 
             if len(strips) > 0:
-                strip_width = (target[1][1] - target[0][1]) * size
-                self.make_target_image(date_dir, cur_date, strips, strip_width, size, force_creation=force_creation)
+                strip_width = (target[1][1] - target[0][1]) * self.size
+                self._make_target_image(strips, strip_width)
 
-            cur_date += step
+            self.cur_date += step
 
 
-Downloader().main(create_annotated=True, force_creation=True)
+Downloader(create_annotated=True, force_creation=True).run()
