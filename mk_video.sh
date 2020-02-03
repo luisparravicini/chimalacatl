820552e9da09675eea103399b5e89131d89ec41d
Luis Parravicini <lparravi@gmail.com>
2020-02-03 14:55:21 -0300

    framerate

--- mk_video.sh
+++ mk_video.sh
@@ -3,4 +3,4 @@
 
 find ~/cache-sat/ -iname '*target*.jpg' | sed -e "s/$/'/" -e "s/^/file '/" >targets.txt
 
-ffmpeg -f concat -safe 0 -i targets.txt -y -r 5 -c:v libx264 target.mp4
+ffmpeg -f concat -safe 0 -i targets.txt -y -r 10 -c:v libx264 target.mp4
