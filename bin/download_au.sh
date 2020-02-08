dd70dcea6b2ec0baa8faa5458e1de64db711ccfa
Luis Parravicini <lparravi@gmail.com>
2020-02-08 20:34:09 -0300

    using depth 8 as default

--- bin/download_au.sh
+++ bin/download_au.sh
@@ -3,4 +3,10 @@
 dir=`dirname "$0"`
 source "$dir"/common.sh
 
-python "$dir"/../chimalacatl.py --depth $depth --target "12 5 17 13" --location "$lat $lng" --date $*
+# for depth 20:
+# --target "12 5 17 13"
+#
+# for depth 8:
+# --target "5 2 6 5"
+
+python "$dir"/../chimalacatl.py --depth $depth --target "5 2 6 5" --location "$lat $lng" --date $*
